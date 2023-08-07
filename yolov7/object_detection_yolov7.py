a23897bd9c99dd1c440bb8801857c4adf73c95dd
sajal <sajal.rastogi@doselect.com>
2023-08-07 11:45:38 +0530

    update for object detection wrapper

--- yolov7/object_detection_yolov7.py
+++ yolov7/object_detection_yolov7.py
@@ -21,7 +21,7 @@ class Yolov7():
     device = None
     convert_image = None
     model = None
-    objects_to_be_detected = []
+    objects_detected_confidence_mapping = []
 
     def __init__(self, objects_to_be_detected=None):
         self.image_size = ObjectDetectionConstants.OBJECT_DETECTION_IMAGE_SIZE.value
@@ -32,15 +32,26 @@ class Yolov7():
         self.model = attempt_load([model_path + f'/{ObjectDetectionConstants.OBJECT_DETECTION_MODEL_NAME.value}'], 
                                   map_location=self.device)
         if objects_to_be_detected is None:
-            objects_to_be_detected = copy.deepcopy(ObjectDetectionConstants.OBJECT_DETECTION_OBJECTS.value)
+            self.objects_detected_confidence_mapping = copy.deepcopy(ObjectDetectionConstants.OBJECT_DETECTION_OBJECTS.value)
+        else:
+            self.objects_detected_confidence_mapping = self.get_object_confidence_mapping(objects_to_be_detected)
         self.objects_to_be_detected = objects_to_be_detected
         self.stride = int(self.model.stride.max())  # model stride
     
+
     def load_model_weights(self):
         file = "/home/sajalrastogi/DoSelect/VideoProctoring/do-proctor-engine-api/src/yolov7.pt"
         file = pathlib.Path(str(file).strip().replace("'", '').lower())
         if not file.exists():
             os.system(f'curl -L {ObjectDetectionConstants.OBJECT_DETECTION_WIEGHTS_PATH.value}')
+
+
+    def get_object_confidence_mapping(self, objects_to_be_detected):
+        objects_detected_confidence_mapping = {}
+        for _object in objects_to_be_detected:
+            objects_detected_confidence_mapping[_object] = []
+        return objects_detected_confidence_mapping
+
 
     def load_image(self, image):
         img = letterbox(image, self.image_size, stride=self.stride)[0]
@@ -49,17 +60,24 @@ class Yolov7():
         img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
         img = np.ascontiguousarray(img)
         return img
-    
-    def detect_object(self, img):
-        # dataset = LoadImages(image_path, img_size=self.image_size, stride=self.stride)
-        # for path, img, im0s, vid_cap in dataset:
-        #     pass
-        
+
+
+    def preprocess_image(self, image):
+        img = letterbox(image, 640, stride=32)[0]
+        img = img[:, :, ::-1].transpose(2, 0, 1)
+        img = np.ascontiguousarray(img)
         img = torch.from_numpy(img).to(self.device)
-        img = img.half() if self.half else img.float()  # uint8 to fp16/32
+        img = img.half() if self.convert_image else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
+
+        return img
+
+
+    def detect_object(self, img):
+
+        img = self.preprocess_image(image=img)
 
         with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
             prediction = self.model(img, augment=ObjectDetectionConstants.OBJECT_DETECTION_OPT.value['augment'])[0]
@@ -77,9 +95,9 @@ class Yolov7():
 
         for i, item in enumerate(prediction):
             label = item[-1]
-            if label in list(self.objects_to_be_detected.keys()):
+            if label in list(self.objects_detected_confidence_mapping.keys()):
                 # item list containes boundary box till index 4 and index 4 is confidence
-                self.objects_to_be_detected[label].append({'bbox': item[:4], 'confidence': item[4]})
-        # return boxes, confidences, labels
-        return self.objects_to_be_detected
+                self.objects_detected_confidence_mapping[label].append({'bbox': item[:4], 'confidence': item[4]})
+        # return {object: [{confidence: , bbox: }]}
+        return self.objects_detected_confidence_mapping
 
